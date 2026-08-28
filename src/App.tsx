b066083b61cbfc68d1e48a67d8d8df338a6100f9
allyuh <mejiaallya00@gmail.com>
2026-08-28 18:20:23 +0800

    scroll fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,54 @@
 import Navbar from "./components/Navbar";
 import "./App.css";
 import Home from "./components/Home";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [collectionFixed, setCollectionFixed] = useState(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const transitionPoint = window.innerHeight;
+
+      if (window.scrollY >= transitionPoint) {
+        setCollectionFixed(false);
+      } else {
+        setCollectionFixed(true);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    handleScroll();
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <div className="page">
       <Navbar />
 
       <main>
+        {/* HOME */}
         <div className="home-wrapper">
           <Home />
         </div>
 
-        <section id="collection" className="collection">
-          <h2>Collection</h2>
-        </section>
+        {/* COLLECTION */}
+        <div className="collection-wrapper">
+          <section
+            id="collection"
+            className={`collection ${
+              collectionFixed ? "collection-fixed" : "collection-normal"
+            }`}
+          >
+            <h2>Collection</h2>
+          </section>
+        </div>
 
+        {/* NORMAL SCROLLING SECTIONS */}
         <section id="videos" className="videos">
           <h2>Videos</h2>
         </section>
